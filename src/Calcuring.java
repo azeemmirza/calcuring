eef02ed540101931ea0b9b330f5b2cd8a46e7081
Azeem Mirza <contact@azeemirza.com>
2018-06-30 21:55:45 +0500

    Main file updated

---
+++ src/Calcuring.java
@@ -0,0 +1,12 @@
+/**
+ * @author      Azeem Mirza <contact@azeemirza.com>
+ * @version     0.1.1
+ * @since       0.1.0
+ * @date        31-5-2017
+ */
+public class Calcuring {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
