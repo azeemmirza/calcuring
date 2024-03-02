c9e82217b80d9b45340f4006143ce9ec3ea9b015
Azeem Mirza <azeem.mirza94@gmail.com>
2024-03-02 17:05:28 +0500

    added docs for main file

--- __main__.py
+++ __main__.py
@@ -1,10 +1,11 @@
 from calcuring import Calcuring
 
+# to take user input
 operand_one = input('Enter number: ')
 operator = input('Enter Op (+ - * /): ')
 operand_two = input('Enter number: ')
 
-# input string formation
+# concatenated operands on the sides fo the operator, to form input string
 input_str = operand_one + operator + operand_two
 
 res = Calcuring().calculate(input_str)
